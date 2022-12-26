227499b00e936b3de07f949caf536a684c19464e
Hary Suryanto <hary.suryanto01@gmail.com>
2022-12-27 01:10:49 +0700

    Implement feature of Show Balance in HomeScreen

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -8,21 +8,31 @@ import {
 import {colors} from '../../utils/colors';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import AppBar from '../../components/AppBar';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {
-  faMoneyBillTransfer,
-  IconDefinition,
-} from '@fortawesome/free-solid-svg-icons';
-import Gap from '../../components/Gap';
+import {faMoneyBillTransfer} from '@fortawesome/free-solid-svg-icons';
 import {MenuGridTile} from './components/MenuGridTile';
+import {useContext} from 'react';
+import {CashListContext} from '../../contexts/CashContext';
+import {CashType} from '../../interfaces/cash';
 
 const HomeScreen = (props: any) => {
+  const cashListContext = useContext(CashListContext);
+  const getBalance = () => {
+    let cashList = cashListContext.cashList;
+    let totalCashIn = cashList
+      .filter(cash => cash.type === CashType.In)
+      .reduce((sum, current) => sum + current.amount, 0);
+    let totalCashOut = cashList
+      .filter(cash => cash.type === CashType.Out)
+      .reduce((sum, current) => sum + current.amount, 0);
+    return totalCashIn - totalCashOut;
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <AppBar />
         <View style={styles.balanceContainer}>
-          <Text style={styles.balance}>Rp230.000</Text>
+          <Text style={styles.balance}>{`Rp ${getBalance()}`}</Text>
           <Text style={styles.title}>Saldo</Text>
         </View>
         <View style={{flexDirection: 'row', marginHorizontal: 30}}>
