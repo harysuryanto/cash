e1bb61be3e6293ebace92cb8d698ed29459abcfa
Hary Suryanto <hary.suryanto01@gmail.com>
2024-08-11 11:44:31 +0700

    Setup environment and scripts

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
+const ENV = (process.env.ENVIRONMENT || "development").toLowerCase();
+
 module.exports = function (api) {
   api.cache(true);
+  console.info("🌏 Using .env." + ENV + " for babel config");
+
+  const plugins = [
+    [
+      "module:react-native-dotenv",
+      {
+        envName: "ENVIRONMENT",
+        moduleName: "@env",
+        path: ".env." + ENV,
+      },
+    ],
+  ];
+
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins,
   };
 };
---
+++ env.d.ts
@@ -0,0 +1,4 @@
+declare module "@env" {
+  export const ENVIRONMENT: string;
+  export const GREETING: string;
+}
