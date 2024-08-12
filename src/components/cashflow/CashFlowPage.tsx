0b97c696158f40e2cb6d0067e8c742c907630510
Hary Suryanto <hary.suryanto01@gmail.com>
2024-08-12 15:55:59 +0700

    Unuse expo-router in CashFlowPage.tsx

--- src/components/cashflow/CashFlowPage.tsx
+++ src/components/cashflow/CashFlowPage.tsx
@@ -1,10 +1,7 @@
-import { useRouter } from "expo-router";
 import { StyleSheet, View } from "react-native";
 import TransactionsList from "./TransactionsList";
 
 export default function CashFlowPage() {
-  const router = useRouter();
-
   return (
     <View style={styles.container}>
       <TransactionsList />
