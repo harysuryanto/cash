fea5a3b77934d457bdad927ea7878b9efc6456b6
Hary Suryanto <hary.suryanto01@gmail.com>
2024-08-12 10:19:31 +0700

    Set page animation to slide_from_right

--- app/(private)/_layout.tsx
+++ app/(private)/_layout.tsx
@@ -3,7 +3,9 @@ import React from "react";
 
 export default function Layout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
+    <Stack
+      screenOptions={{ animation: "slide_from_right", headerShown: false }}
+    >
       <Stack.Screen name="index" />
       <Stack.Screen name="cash-flow" />
     </Stack>
