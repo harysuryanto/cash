9ff500f3fa4812f7ce92ddab2a60d8d907aa8578
Hary Suryanto <hary.suryanto01@gmail.com>
2024-09-17 01:57:49 +0700

    Implement Add Transaction Feature (WIP)

--- src/components/add-transaction/AddTransactionForm.tsx
+++ src/components/add-transaction/AddTransactionForm.tsx
@@ -1,24 +1,52 @@
 import { View } from "react-native";
-import React from "react";
-import StyledTextInput from "@/src/components/shared/StyledTextInput";
+import React, { useState } from "react";
+import { Input } from "@/src/components/shared/nativecn/Input";
+import { Select } from "@/src/components/shared/nativecn/Select";
+import StyledDateTimePickerButton from "../shared/StyledDateTimePickerButton";
 
 export default function AddTransactionForm() {
+  // TODO: Add form validation and form submission
+
+  const [selectedOption, setSelectedOption] = useState<
+    string | number | undefined
+  >();
+
   return (
     <View className="gap-1">
-      <StyledTextInput
-        label="Type (income/expense)"
-        keyboardType="numeric"
-        required
+      <Select
+        label="Type"
+        options={[
+          { id: 1, name: "Expense" },
+          { id: 2, name: "Income" },
+        ]}
+        labelKey="name"
+        valueKey="id"
+        selectedValue={selectedOption}
+        onSelect={(value) => setSelectedOption(value)}
       />
-      <StyledTextInput label="Amount" keyboardType="numeric" required />
-      <StyledTextInput label="Category" keyboardType="default" required />
-      <StyledTextInput
-        label="Source of funds"
-        keyboardType="default"
-        required
+      <Input label="Amount" keyboardType="numeric" />
+      <Select
+        label="Category"
+        options={[
+          { id: 1, name: "Basic Need" },
+          { id: 2, name: "Desire" },
+          { id: 3, name: "Investment" },
+        ]}
+        labelKey="name"
+        valueKey="id"
+        selectedValue={selectedOption}
+        onSelect={(value) => setSelectedOption(value)}
       />
-      <StyledTextInput label="Date" keyboardType="default" required />
-      <StyledTextInput
+      <Select
+        label="Fund"
+        options={[{ id: 1, name: "Cash" }]}
+        labelKey="name"
+        valueKey="id"
+        selectedValue={selectedOption}
+        onSelect={(value) => setSelectedOption(value)}
+      />
+      <StyledDateTimePickerButton label="Date" onChange={console.log} />
+      <Input
         label="Notes"
         keyboardType="default"
         multiline
--- src/components/add-transaction/AddTransactionPage.tsx
+++ src/components/add-transaction/AddTransactionPage.tsx
@@ -1,11 +1,13 @@
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import React from "react";
 import AddTransactionForm from "./AddTransactionForm";
 
 export default function AddTransactionPage() {
   return (
-    <View className="flex-1 p-3 bg-white">
-      <AddTransactionForm />
-    </View>
+    <ScrollView className="flex-1 bg-white">
+      <View className="p-3">
+        <AddTransactionForm />
+      </View>
+    </ScrollView>
   );
 }
