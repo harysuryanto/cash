93e98bb7b47ef7203134342949dcd0fd03411a87
Hary Suryanto <hary.suryanto01@gmail.com>
2024-10-28 02:11:23 +0700

    Finish Add Transaction Form with temporary submit button

--- src/components/add-transaction/AddTransactionForm.tsx
+++ src/components/add-transaction/AddTransactionForm.tsx
@@ -1,69 +1,136 @@
-import { View } from "react-native";
-import React, { useState } from "react";
+import { Button, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import { Input } from "@/src/components/shared/nativecn/Input";
 import { Select } from "@/src/components/shared/nativecn/Select";
 import StyledDateTimePickerButton from "@/src/components/shared/StyledDateTimePickerButton";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Transaction } from "@/src/types/Transaction";
+import { Timestamp } from "firebase/firestore";
+import { addTransaction } from "@/src/services/transaction";
+import { useRouter } from "expo-router";
+import { createTransactionsListQueryKey } from "@/src/hooks/useTransactionsList";
 
 export default function AddTransactionForm() {
+  const router = useRouter();
+
   // TODO: Add form validation and form submission
 
-  const [type, setSelectedOption] = useState<string | number | undefined>();
+  const categories: Record<
+    "expense" | "income",
+    { id: number; name: string }[]
+  > = {
+    expense: [
+      { id: 1, name: "Basic Need" },
+      { id: 2, name: "Desire" },
+      { id: 3, name: "Investment" },
+      { id: 4, name: "Uncategorized" },
+    ],
+    income: [
+      { id: 1, name: "Salary" },
+      { id: 2, name: "Bonus" },
+      { id: 3, name: "Investment Return" },
+      { id: 4, name: "Uncategorized" },
+    ],
+  };
+
+  const funds: { id: number; name: string }[] = [{ id: 1, name: "Cash" }];
+
+  const [type, setType] = useState<"expense" | "income" | undefined>();
   const [nominal, setNominal] = useState<string>();
-  const [category, setCategory] = useState<string | number | undefined>();
-  const [fund, setFund] = useState<string | number | undefined>();
+  const [categoryId, setCategoryId] = useState<number | undefined>();
+  const [fundId, setFundId] = useState<number | undefined>(1);
   const [date, setDate] = useState<Date>(new Date());
   const [description, setDescription] = useState<string>();
+
+  const queryClient = useQueryClient();
+  const { mutate, isPending, error } = useMutation({
+    mutationFn: async (data: Omit<Transaction, "id">) => {
+      return await addTransaction(data);
+    },
+    onSuccess: async () => {
+      router.back();
+      await queryClient.invalidateQueries({
+        queryKey: createTransactionsListQueryKey(),
+      });
+    },
+  });
+
+  useEffect(() => {
+    setCategoryId(undefined);
+  }, [type]);
+
+  useEffect(() => {
+    if (error) console.log(error);
+  }, [error]);
+
+  const onSubmit = async () => {
+    if (!type || !nominal || !categoryId || !fundId || !date) {
+      return;
+    }
+
+    mutate({
+      type: type,
+      category: categories[type].find(({ id }) => id === Number(categoryId))!
+        .name,
+      date: Timestamp.fromDate(date),
+      description: description ?? "",
+      fund: funds.find(({ id }) => id === Number(fundId))!.name,
+      nominal: Number(nominal),
+    });
+  };
 
   return (
     <View className="gap-1">
       <Select
         label="Type"
-        placeholder=""
+        placeholder="Select type"
         options={[
-          { id: 1, name: "Expense" },
-          { id: 2, name: "Income" },
+          { id: "expense", name: "Expense" },
+          { id: "income", name: "Income" },
         ]}
         labelKey="name"
         valueKey="id"
         selectedValue={type}
-        onSelect={(value) => setSelectedOption(value)}
+        onSelect={(value) => setType(value as Transaction["type"])}
       />
       <Input
-        label="Amount"
+        label="Nominal"
+        placeholder="Nominal"
         value={nominal}
         onChangeText={setNominal}
         keyboardType="numeric"
       />
-      <Select
-        label="Category"
-        placeholder=""
-        options={[
-          { id: 1, name: "Basic Need" },
-          { id: 2, name: "Desire" },
-          { id: 3, name: "Investment" },
-        ]}
-        labelKey="name"
-        valueKey="id"
-        selectedValue={category}
-        onSelect={(value) => setCategory(value)}
-      />
+      {Boolean(type) ? (
+        <Select
+          label="Category"
+          placeholder="Select category"
+          options={type === "income" ? categories.income : categories.expense}
+          labelKey="name"
+          valueKey="id"
+          selectedValue={categoryId}
+          onSelect={(value) => setCategoryId(value as number)}
+        />
+      ) : (
+        <></>
+      )}
       <Select
         label="Fund"
-        placeholder=""
+        placeholder="Select fund"
         options={[{ id: 1, name: "Cash" }]}
         labelKey="name"
         valueKey="id"
-        selectedValue={fund}
-        onSelect={(value) => setFund(value)}
+        selectedValue={fundId}
+        onSelect={(value) => setFundId(value as number)}
       />
       <StyledDateTimePickerButton
         label="Date"
-        placeholder=""
+        placeholder="Select date"
         date={date}
         onChange={({ date }) => setDate(new Date(date!.toString()))}
       />
       <Input
         label="Notes"
+        placeholder="Add notes"
         value={description}
         onChangeText={setDescription}
         keyboardType="default"
@@ -71,6 +138,7 @@ export default function AddTransactionForm() {
         numberOfLines={3}
         textAlignVertical="top"
       />
+      <Button title="Submit" onPress={onSubmit} disabled={isPending} />
     </View>
   );
 }
