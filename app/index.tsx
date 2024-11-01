efc0d87b5294ca3655c4400eb7e66a87f3248a31
Hary Suryanto <hary.suryanto01@gmail.com>
2024-11-02 02:54:22 +0700

    Temporarily change starting route to /cash-flow for easier development

--- app/index.tsx
+++ app/index.tsx
@@ -2,5 +2,6 @@ import { Redirect } from "expo-router";
 import React from "react";
 
 export default function RootRoute() {
-  return <Redirect href="/(private)" />;
+  // return <Redirect href="/(private)" />;
+  return <Redirect href="/(private)/cash-flow" />;
 }
