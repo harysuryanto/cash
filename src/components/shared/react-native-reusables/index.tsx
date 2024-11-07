6bb292543e488b3ce30c92abde92cc6ef569156e
Hary Suryanto <hary.suryanto01@gmail.com>
2024-11-08 00:43:27 +0700

    Create index.tsx

---
+++ src/components/shared/react-native-reusables/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./Button";
+import { Card } from "./Card";
+import { Dialog } from "./Dialog";
+import { Input } from "./Input";
+import { Label } from "./Label";
+import { Select } from "./Select";
+import { Text } from "./Text";
+import { Textarea } from "./Textarea";
+
+export { Button, Card, Dialog, Input, Label, Select, Text, Textarea };
