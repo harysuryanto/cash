49977ac8843cc3e881e4ee7820c26190f9daa963
Hary Suryanto <hary.suryanto01@gmail.com>
2024-11-17 23:28:07 +0700

    Extract providers-related codes in _layout.tsx to new Providers.tsx file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,20 @@
 import "expo-dev-client";
 import "@/src/global.css";
 import { checkOtaUpdate } from "@/src/utils/utils/ota-update";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SplashScreen, Stack } from "expo-router";
-import { Theme, ThemeProvider } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import { Platform } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { NAV_THEME } from "@/src/constants";
-import { useColorScheme } from "@/src/hooks/useColorScheme";
-import { PortalHost } from "@/src/components/shared/react-native-reusables/primitives/portal";
-import { AddTransactionFormProvider } from "@/src/contexts/AddTransactionFormContext";
-import { EditTransactionFormProvider } from "@/src/contexts/EditTransactionFormContext";
-import { AuthProvider } from "@/src/contexts/AuthContext";
+import { useEffect } from "react";
+import Providers from "@/src/components/Providers";
 
-const LIGHT_THEME: Theme = {
-  dark: false,
-  colors: NAV_THEME.light,
-};
-const DARK_THEME: Theme = {
-  dark: true,
-  colors: NAV_THEME.dark,
-};
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    mutations: { throwOnError: false, retry: false },
-    queries: { throwOnError: false, retry: false },
-  },
-});
+function Layout() {
+  // Load all stuff here that needs to run only once at the beginning.
+  useEffect(() => {
+    SplashScreen.hideAsync();
+    checkOtaUpdate();
+  }, []);
 
-function Nav() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(private)" />
@@ -39,67 +22,10 @@ function Nav() {
   );
 }
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-function RootLayout() {
-  useEffect(() => {
-    SplashScreen.hideAsync();
-    checkOtaUpdate();
-  }, []);
-
-  return <Nav />;
-}
-
-export default function RootLayoutNav() {
-  const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme();
-  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      const theme = await AsyncStorage.getItem("theme");
-      if (Platform.OS === "web") {
-        // Adds the background color to the html element to prevent white background on overscroll.
-        document.documentElement.classList.add("bg-background");
-      }
-      if (!theme) {
-        AsyncStorage.setItem("theme", colorScheme);
-        setIsColorSchemeLoaded(true);
-        return;
-      }
-      const colorTheme = theme === "dark" ? "dark" : "light";
-      if (colorTheme !== colorScheme) {
-        setColorScheme(colorTheme);
-
-        setIsColorSchemeLoaded(true);
-        return;
-      }
-      setIsColorSchemeLoaded(true);
-    })().finally(() => {
-      SplashScreen.hideAsync();
-    });
-  }, []);
-
-  if (!isColorSchemeLoaded) {
-    return null;
-  }
-
+export default function Root() {
   return (
-    <>
-      <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
-        <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
-        <QueryClientProvider client={queryClient}>
-          <AuthProvider>
-            <AddTransactionFormProvider>
-              <EditTransactionFormProvider>
-                <RootLayout />
-              </EditTransactionFormProvider>
-            </AddTransactionFormProvider>
-          </AuthProvider>
-        </QueryClientProvider>
-      </ThemeProvider>
-      {/* Default Portal Host (one per app) */}
-      <PortalHost />
-    </>
+    <Providers>
+      <Layout />
+    </Providers>
   );
 }
---
+++ src/components/Providers.tsx
@@ -0,0 +1,82 @@
+import React, { PropsWithChildren, useEffect, useState } from "react";
+import { Platform } from "react-native";
+import { SplashScreen } from "expo-router";
+import { Theme, ThemeProvider } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { NAV_THEME } from "@/src/constants";
+import { useColorScheme } from "@/src/hooks/useColorScheme";
+import { PortalHost } from "@/src/components/shared/react-native-reusables/primitives/portal";
+import { AuthProvider } from "@/src/contexts/AuthContext";
+import { AddTransactionFormProvider } from "@/src/contexts/AddTransactionFormContext";
+import { EditTransactionFormProvider } from "@/src/contexts/EditTransactionFormContext";
+
+const LIGHT_THEME: Theme = {
+  dark: false,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  dark: true,
+  colors: NAV_THEME.dark,
+};
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    mutations: { throwOnError: false, retry: false },
+    queries: { throwOnError: false, retry: false },
+  },
+});
+
+export default function Providers({ children }: PropsWithChildren) {
+  const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const theme = await AsyncStorage.getItem("theme");
+      if (Platform.OS === "web") {
+        // Adds the background color to the html element to prevent white background on overscroll.
+        document.documentElement.classList.add("bg-background");
+      }
+      if (!theme) {
+        AsyncStorage.setItem("theme", colorScheme);
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      const colorTheme = theme === "dark" ? "dark" : "light";
+      if (colorTheme !== colorScheme) {
+        setColorScheme(colorTheme);
+
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      setIsColorSchemeLoaded(true);
+    })().finally(() => {
+      SplashScreen.hideAsync();
+    });
+  }, []);
+
+  if (!isColorSchemeLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+        <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <AddTransactionFormProvider>
+              <EditTransactionFormProvider>
+                {children}
+              </EditTransactionFormProvider>
+            </AddTransactionFormProvider>
+          </AuthProvider>
+        </QueryClientProvider>
+      </ThemeProvider>
+      {/* Default Portal Host (one per app) */}
+      <PortalHost />
+    </>
+  );
+}
