87dfe29372074e8917a06d7cb58b5f768b7e2c99
Hary Suryanto <hary.suryanto01@gmail.com>
2024-11-19 00:02:47 +0700

    Set a screen animation to "ios"

--- app/(public)/_layout.tsx
+++ app/(public)/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function Layout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
+    <Stack screenOptions={{ animation: "ios", headerShown: false }}>
       <Stack.Screen name="sign-in" />
     </Stack>
   );
