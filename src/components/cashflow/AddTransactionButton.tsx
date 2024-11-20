cce24c84c1e7716c773c63611c629b84bac6fd4d
Hary Suryanto <hary.suryanto01@gmail.com>
2024-11-21 01:47:53 +0700

    Set icon color in AddTransactionButton

--- src/components/cashflow/AddTransactionButton.tsx
+++ src/components/cashflow/AddTransactionButton.tsx
@@ -2,19 +2,23 @@ import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { Link } from "expo-router";
+import { useColorScheme } from "@/src/hooks/useColorScheme";
 
-interface AddTransactionButtonProps extends TouchableOpacityProps {
-  color?: string | undefined;
-}
+interface AddTransactionButtonProps extends TouchableOpacityProps {}
 
 export default function AddTransactionButton({
-  color,
   ...rest
 }: AddTransactionButtonProps) {
+  const { isDarkColorScheme } = useColorScheme();
+
   return (
     <Link href="/(private)/cash-flow/add" asChild>
       <TouchableOpacity hitSlop={16} {...rest}>
-        <Ionicons name="add" size={24} color={color} />
+        <Ionicons
+          name="add"
+          size={24}
+          color={isDarkColorScheme ? "#fafafa" : "#0a0a0b"}
+        />
       </TouchableOpacity>
     </Link>
   );
