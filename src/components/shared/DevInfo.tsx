39f8eec0f9119509b176eb039714b9fb12f89219
Hary Suryanto <hary.suryanto01@gmail.com>
2024-11-23 01:33:58 +0700

    Update data in DevInfo

--- src/components/shared/DevInfo.tsx
+++ src/components/shared/DevInfo.tsx
@@ -3,18 +3,19 @@ import React from "react";
 import Constants from "expo-constants";
 import { Text } from "@/src/components/shared/react-native-reusables/Text";
 import { cn } from "@/src/utils/utils/utils";
+import { env } from "@/src/utils/utils/env";
 
 type DevInfoProps = ViewProps & {
   enabled?: boolean;
 };
 
 export default function DevInfo({
-  enabled = process.env.NODE_ENV === "development",
+  enabled = env.EXPO_PUBLIC_APP_ENV !== "production" &&
+    process.env.NODE_ENV !== "production",
   ...rest
 }: DevInfoProps) {
   if (!enabled) return null;
 
-  const processEnv = JSON.stringify(process.env);
   const expoConfigExtra = JSON.stringify(Constants.expoConfig?.extra);
   // @ts-expect-error
   const easConfigExtra = JSON.stringify(Constants.easConfig?.extra);
@@ -22,16 +23,20 @@ export default function DevInfo({
   return (
     <View {...rest} className={cn("gap-2 p-4", rest.className)}>
       <View className="justify-between p-2 rounded-lg bg-primary-foreground">
-        <Text className="font-semibold">process.env</Text>
-        <Text>{processEnv}</Text>
-      </View>
-      <View className="justify-between p-2 rounded-lg bg-primary-foreground">
         <Text className="font-semibold">Constants.expoConfig.extra</Text>
         <Text>{expoConfigExtra}</Text>
       </View>
       <View className="justify-between p-2 rounded-lg bg-primary-foreground">
         <Text className="font-semibold">Constants.easConfig.extra</Text>
         <Text>{easConfigExtra}</Text>
+      </View>
+      <View className="justify-between p-2 rounded-lg bg-primary-foreground">
+        <Text className="font-semibold">env.EXPO_PUBLIC_APP_ENV</Text>
+        <Text>{env.EXPO_PUBLIC_APP_ENV || "not found"}</Text>
+      </View>
+      <View className="justify-between p-2 rounded-lg bg-primary-foreground">
+        <Text className="font-semibold">process.env.EXPO_PUBLIC_APP_ENV</Text>
+        <Text>{process.env.EXPO_PUBLIC_APP_ENV || "not found"}</Text>
       </View>
     </View>
   );
