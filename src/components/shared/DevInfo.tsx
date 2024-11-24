e01243d8385c4fc8d2ba86d2b150968aaecc7157
Hary Suryanto <hary.suryanto01@gmail.com>
2024-11-24 22:54:31 +0700

    Remove info from expo-constants package

--- src/components/shared/DevInfo.tsx
+++ src/components/shared/DevInfo.tsx
@@ -1,6 +1,5 @@
 import { Platform, View, ViewProps } from "react-native";
 import React from "react";
-import Constants from "expo-constants";
 import { Text } from "@/src/components/shared/react-native-reusables/Text";
 import { cn } from "@/src/utils/utils/utils";
 import { env } from "@/src/utils/utils/env";
@@ -15,20 +14,8 @@ export default function DevInfo({
 }: DevInfoProps) {
   if (!enabled) return null;
 
-  const expoConfigExtra = JSON.stringify(Constants.expoConfig?.extra);
-  // @ts-expect-error
-  const easConfigExtra = JSON.stringify(Constants.easConfig?.extra);
-
   return (
     <View {...rest} className={cn("gap-2 p-4", rest.className)}>
-      <View className="justify-between p-2 rounded-lg bg-primary-foreground">
-        <Text className="font-semibold">Constants.expoConfig.extra</Text>
-        <Text>{expoConfigExtra}</Text>
-      </View>
-      <View className="justify-between p-2 rounded-lg bg-primary-foreground">
-        <Text className="font-semibold">Constants.easConfig.extra</Text>
-        <Text>{easConfigExtra}</Text>
-      </View>
       <View className="justify-between p-2 rounded-lg bg-primary-foreground">
         <Text className="font-semibold">env.EXPO_PUBLIC_APP_ENV</Text>
         <Text>{env.EXPO_PUBLIC_APP_ENV || "not found"}</Text>
