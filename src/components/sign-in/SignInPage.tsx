9da0b51d6da51f6fdc8e5df9804cee718162e86e
Hary Suryanto <hary.suryanto01@gmail.com>
2024-11-25 16:33:47 +0700

    Remove argument of enabled in DevInfo

--- src/components/sign-in/SignInPage.tsx
+++ src/components/sign-in/SignInPage.tsx
@@ -8,8 +8,6 @@ import React from "react";
 import SignInForm from "./SignInForm";
 import DevInfo from "@/src/components/shared/DevInfo";
 
-const appEnv = process.env.EXPO_PUBLIC_APP_ENV;
-
 export default function SignInPage() {
   return (
     <SafeAreaView className="flex-1">
@@ -19,10 +17,7 @@ export default function SignInPage() {
       >
         <ScrollView className="flex-1 p-4">
           <SignInForm />
-          <DevInfo
-            enabled={appEnv === "development" || appEnv === "staging"}
-            className="mt-4"
-          />
+          <DevInfo className="mt-4" />
         </ScrollView>
       </KeyboardAvoidingView>
     </SafeAreaView>
