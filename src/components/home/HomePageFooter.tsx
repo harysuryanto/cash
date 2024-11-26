cfb4e474788127db35e0b801ffc57f725c66dd03
Hary Suryanto <hary.suryanto01@gmail.com>
2024-11-26 16:17:27 +0700

    Update UI in HomePageFooter

--- src/components/home/HomePageFooter.tsx
+++ src/components/home/HomePageFooter.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { env } from "@/src/utils/utils/env";
-import Constants from "expo-constants";
 import { Text } from "@/src/components/shared/react-native-reusables";
+import Constants from "expo-constants";
+import { env } from "@/src/utils/utils/env";
 
 export default function HomePageFooter() {
+  const appEnv =
+    env.EXPO_PUBLIC_APP_ENV !== "production"
+      ? ` - ${env.EXPO_PUBLIC_APP_ENV}`
+      : "";
+
   return (
-    <Text style={{ width: "100%", textAlign: "center" }}>
-      v{Constants.expoConfig?.version} - {env.EXPO_PUBLIC_APP_ENV}
+    <Text className="text-sm text-center text-muted-foreground">
+      v{Constants.expoConfig?.version}
+      {appEnv}
     </Text>
   );
 }
