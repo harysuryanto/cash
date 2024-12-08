812d2ab15b386ac50d6ce8446e4f950ac568b416
Hary Suryanto <hary.suryanto01@gmail.com>
2024-12-08 22:38:03 +0700

    Set iosClientId, androidClientId, and webClientId for prod

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -16,6 +16,7 @@ import * as AuthService from "@/src/services/auth";
 import { FIREBASE_AUTH } from "@/firebaseConfig";
 import * as Google from "expo-auth-session/providers/google";
 import { signInWithCredential } from "@/src/services/auth";
+import { env } from "@/src/utils/utils/env";
 
 interface AuthContextValue {
   user?: User | null;
@@ -57,15 +58,30 @@ export const AuthProvider = ({ children }: PropsWithChildren) => {
     return unsubscribe;
   }, []);
 
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    iosClientId:
-      "598165881469-6n51m0e246l9uj91ug2bpgt989ktk6rr.apps.googleusercontent.com",
-    androidClientId:
-      "598165881469-cnd0jpn5dm1qrpcto7gsik045b7ro8nn.apps.googleusercontent.com",
-    webClientId:
-      // "59214547434392-knejs1im3t07pl48abjb0rqstks6hka1.apps.googleusercontent.com",
-      "214547434392-knejs1im3t07pl48abjb0rqstks6hka1.apps.googleusercontent.com",
-  });
+  const [request, response, promptAsync] = Google.useAuthRequest(
+    env.EXPO_PUBLIC_APP_ENV === "development"
+      ? {
+          iosClientId:
+            "598165881469-6n51m0e246l9uj91ug2bpgt989ktk6rr.apps.googleusercontent.com",
+          androidClientId:
+            "598165881469-cnd0jpn5dm1qrpcto7gsik045b7ro8nn.apps.googleusercontent.com",
+          webClientId:
+            // "59214547434392-knejs1im3t07pl48abjb0rqstks6hka1.apps.googleusercontent.com",
+            "214547434392-knejs1im3t07pl48abjb0rqstks6hka1.apps.googleusercontent.com",
+        }
+      : env.EXPO_PUBLIC_APP_ENV === "staging"
+      ? {}
+      : // production
+        {
+          iosClientId:
+            "598165881469-vbsbk0m7chan4nlhk4abcq9dbk0aoal6.apps.googleusercontent.com",
+          androidClientId:
+            "598165881469-gc0pede2bai8lkn88ulh295a4t3cvh2f.apps.googleusercontent.com",
+          webClientId:
+            // "59214547434392-knejs1im3t07pl48abjb0rqstks6hka1.apps.googleusercontent.com",
+            "214547434392-knejs1im3t07pl48abjb0rqstks6hka1.apps.googleusercontent.com",
+        }
+  );
 
   // useEffect(() => {
   //   if (response?.type === "success") {
